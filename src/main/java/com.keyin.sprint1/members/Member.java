6cf66564cd860f5f6cdea1adee47b8696911e77c
dev-brad73 <brad.rice@keyin.com>
2022-02-24 23:30:00 -0330

    Added Member and Tournament Class constructors, getters, and setters

--- src/main/java/com.keyin.sprint1/members/Member.java
+++ src/main/java/com.keyin.sprint1/members/Member.java
@@ -2,4 +2,144 @@ package com.keyin.sprint1.members;
 
 public class Member {
 
+    private Long MemberId;
+    private String MemberName;
+    private String MemberAddress;
+    private String MemberEmail;
+    private String MemberPhoneNumber;
+    private String MembershipStartDate;
+    private String MembershipDuration;
+    private String MembershipType;
+    private String CurrentTournaments;
+    private String PastTournaments;
+    private String UpcomingTournaments;
+
+    public Member() {
+    }
+
+    public Member(Long id,
+                  String memberName,
+                  String memberAddress,
+                  String memberEmail,
+                  String memberPhoneNumber,
+                  String membershipStartDate,
+                  String membershipDuration,
+                  String membershipType,
+                  String currentTournaments,
+                  String pastTournaments,
+                  String upcomingTournaments) {
+        MemberId = id;
+        MemberName = memberName;
+        MemberAddress = memberAddress;
+        MemberEmail = memberEmail;
+        MemberPhoneNumber = memberPhoneNumber;
+        MembershipStartDate = membershipStartDate;
+        MembershipDuration = membershipDuration;
+        MembershipType = membershipType;
+        CurrentTournaments = currentTournaments;
+        PastTournaments = pastTournaments;
+        UpcomingTournaments = upcomingTournaments;
+    }
+
+    public Member(String memberName,
+                  String memberAddress,
+                  String memberEmail,
+                  String memberPhoneNumber,
+                  String membershipStartDate,
+                  String membershipDuration,
+                  String membershipType,
+                  String currentTournaments,
+                  String pastTournaments,
+                  String upcomingTournaments) {
+        MemberName = memberName;
+        MemberAddress = memberAddress;
+        MemberEmail = memberEmail;
+        MemberPhoneNumber = memberPhoneNumber;
+        MembershipStartDate = membershipStartDate;
+        MembershipDuration = membershipDuration;
+        MembershipType = membershipType;
+        CurrentTournaments = currentTournaments;
+        PastTournaments = pastTournaments;
+        UpcomingTournaments = upcomingTournaments;
+    }
+
+    public String getMemberName() {
+        return MemberName;
+    }
+
+    public void setMemberName(String memberName) {
+        MemberName = memberName;
+    }
+
+    public String getMemberAddress() {
+        return MemberAddress;
+    }
+
+    public void setMemberAddress(String memberAddress) {
+        MemberAddress = memberAddress;
+    }
+
+    public String getMemberEmail() {
+        return MemberEmail;
+    }
+
+    public void setMemberEmail(String memberEmail) {
+        MemberEmail = memberEmail;
+    }
+
+    public String getMemberPhoneNumber() {
+        return MemberPhoneNumber;
+    }
+
+    public void setMemberPhoneNumber(String memberPhoneNumber) {
+        MemberPhoneNumber = memberPhoneNumber;
+    }
+
+    public String getMembershipStartDate() {
+        return MembershipStartDate;
+    }
+
+    public void setMembershipStartDate(String membershipStartDate) {
+        MembershipStartDate = membershipStartDate;
+    }
+
+    public String getMembershipDuration() {
+        return MembershipDuration;
+    }
+
+    public void setMembershipDuration(String membershipDuration) {
+        MembershipDuration = membershipDuration;
+    }
+
+    public String getMembershipType() {
+        return MembershipType;
+    }
+
+    public void setMembershipType(String membershipType) {
+        MembershipType = membershipType;
+    }
+
+    public String getCurrentTournaments() {
+        return CurrentTournaments;
+    }
+
+    public void setCurrentTournaments(String currentTournaments) {
+        CurrentTournaments = currentTournaments;
+    }
+
+    public String getPastTournaments() {
+        return PastTournaments;
+    }
+
+    public void setPastTournaments(String pastTournaments) {
+        PastTournaments = pastTournaments;
+    }
+
+    public String getUpcomingTournaments() {
+        return UpcomingTournaments;
+    }
+
+    public void setUpcomingTournaments(String upcomingTournaments) {
+        UpcomingTournaments = upcomingTournaments;
+    }
 }
--- src/main/java/com.keyin.sprint1/tournament/Tournament.java
+++ src/main/java/com.keyin.sprint1/tournament/Tournament.java
@@ -1,4 +1,115 @@
 package com.keyin.sprint1.tournament;
 
 public class Tournament {
+
+    private Long TournamentId;
+    private String TournamentStartDate;
+    private String TournamentEndDate;
+    private String TournamentLocation;
+    private double TournamentEntryFee;
+    private double CashPrizeAmount;
+    private String TournamentParticipants;
+    private String FinalStandings;
+
+    public Tournament() {
+    }
+
+    public Tournament(Long tournamentId,
+                      String tournamentStartDate,
+                      String tournamentEndDate,
+                      String tournamentLocation,
+                      double tournamentEntryFee,
+                      double cashPrizeAmount,
+                      String tournamentParticipants,
+                      String finalStandings) {
+        TournamentId = tournamentId;
+        TournamentStartDate = tournamentStartDate;
+        TournamentEndDate = tournamentEndDate;
+        TournamentLocation = tournamentLocation;
+        TournamentEntryFee = tournamentEntryFee;
+        CashPrizeAmount = cashPrizeAmount;
+        TournamentParticipants = tournamentParticipants;
+        FinalStandings = finalStandings;
+    }
+
+    public Tournament(String tournamentStartDate,
+                      String tournamentEndDate,
+                      String tournamentLocation,
+                      double tournamentEntryFee,
+                      double cashPrizeAmount,
+                      String tournamentParticipants,
+                      String finalStandings) {
+        TournamentStartDate = tournamentStartDate;
+        TournamentEndDate = tournamentEndDate;
+        TournamentLocation = tournamentLocation;
+        TournamentEntryFee = tournamentEntryFee;
+        CashPrizeAmount = cashPrizeAmount;
+        TournamentParticipants = tournamentParticipants;
+        FinalStandings = finalStandings;
+    }
+
+    public Long getTournamentId() {
+        return TournamentId;
+    }
+
+    public void setTournamentId(Long tournamentId) {
+        TournamentId = tournamentId;
+    }
+
+    public String getTournamentStartDate() {
+        return TournamentStartDate;
+    }
+
+    public void setTournamentStartDate(String tournamentStartDate) {
+        TournamentStartDate = tournamentStartDate;
+    }
+
+    public String getTournamentEndDate() {
+        return TournamentEndDate;
+    }
+
+    public void setTournamentEndDate(String tournamentEndDate) {
+        TournamentEndDate = tournamentEndDate;
+    }
+
+    public String getTournamentLocation() {
+        return TournamentLocation;
+    }
+
+    public void setTournamentLocation(String tournamentLocation) {
+        TournamentLocation = tournamentLocation;
+    }
+
+    public double getTournamentEntryFee() {
+        return TournamentEntryFee;
+    }
+
+    public void setTournamentEntryFee(double tournamentEntryFee) {
+        TournamentEntryFee = tournamentEntryFee;
+    }
+
+    public double getCashPrizeAmount() {
+        return CashPrizeAmount;
+    }
+
+    public void setCashPrizeAmount(double cashPrizeAmount) {
+        CashPrizeAmount = cashPrizeAmount;
+    }
+
+    public String getTournamentParticipants() {
+        return TournamentParticipants;
+    }
+
+    public void setTournamentParticipants(String tournamentParticipants) {
+        TournamentParticipants = tournamentParticipants;
+    }
+
+    public String getFinalStandings() {
+        return FinalStandings;
+    }
+
+    public void setFinalStandings(String finalStandings) {
+        FinalStandings = finalStandings;
+    }
 }
+
